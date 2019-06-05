26f1f873378d00b88d1ec7296273c10ec738dc7e
Taraszovics Péter <gtarapeti@gmail.com>
2019-06-05 09:26:17 +0200

    corrected fetch

--- src/main/java/com/codecool/web/dao/database/DatabaseUserDao.java
+++ src/main/java/com/codecool/web/dao/database/DatabaseUserDao.java
@@ -14,7 +14,7 @@ public final class DatabaseUserDao extends AbstractDao implements UserDao {
     }
 
     public List<User> findAll() throws SQLException {
-        String sql = "SELECT id, email, password FROM users";
+        String sql = "SELECT id,username, email, password, role FROM users";
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(sql)) {
             List<User> users = new ArrayList<>();
@@ -26,11 +26,26 @@ public final class DatabaseUserDao extends AbstractDao implements UserDao {
     }
 
     @Override
+    public List<User> findbyRole(boolean role) throws SQLException {
+        String sql = "SELECT * FROM users WHERE role = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setBoolean(1, role);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                List<User> users = new ArrayList<>();
+                while (resultSet.next()) {
+                    users.add(fetchUser(resultSet));
+                }
+                return users;
+            }
+        }
+    }
+
+    @Override
     public User findByEmail(String email) throws SQLException {
         if (email == null || "".equals(email)) {
             throw new IllegalArgumentException("Email cannot be null or empty");
         }
-        String sql = "SELECT id, email, password FROM users WHERE email = ?";
+        String sql = "SELECT * FROM users WHERE email = ?";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, email);
             try (ResultSet resultSet = statement.executeQuery()) {
@@ -44,8 +59,10 @@ public final class DatabaseUserDao extends AbstractDao implements UserDao {
 
     private User fetchUser(ResultSet resultSet) throws SQLException {
         int id = resultSet.getInt("id");
+        String username = resultSet.getString("username");
         String email = resultSet.getString("email");
         String password = resultSet.getString("password");
+        boolean role = resultSet.getBoolean("role");
         return new User(id, email, password);
     }
 }
