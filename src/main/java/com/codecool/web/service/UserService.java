d9a1903a274962472e9807d644c608ed1ec4362b
Taraszovics Péter <gtarapeti@gmail.com>
2019-06-07 12:41:16 +0200

    implemented service methods for daos

--- src/main/java/com/codecool/web/service/UserService.java
+++ src/main/java/com/codecool/web/service/UserService.java
@@ -1,4 +1,15 @@
 package com.codecool.web.service;
 
+import com.codecool.web.model.User;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface UserService {
+    User getByEmail(String email) throws SQLException;
+
+    List<User> getAll() throws SQLException;
+
+    List<User> getbyRole(String role) throws SQLException, ServiceException;
 }
--- src/main/java/com/codecool/web/service/simple/SimpleUsersService.java
+++ src/main/java/com/codecool/web/service/simple/SimpleUsersService.java
@@ -1,6 +1,36 @@
 package com.codecool.web.service.simple;
 
+import com.codecool.web.dao.UserDao;
+import com.codecool.web.model.User;
 import com.codecool.web.service.UserService;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class SimpleUsersService implements UserService {
+    private final UserDao userDao;
+
+    public SimpleUsersService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public User getByEmail(String email) throws SQLException {
+        return userDao.findByEmail(email);
+    }
+
+    @Override
+    public List<User> getAll() throws SQLException {
+        return userDao.findAll();
+    }
+
+    @Override
+    public List<User> getbyRole(String role) throws SQLException, ServiceException {
+        try {
+            return userDao.findbyRole(Boolean.parseBoolean(role));
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
 }
