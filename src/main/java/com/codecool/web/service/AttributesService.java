bf1d1f60c34ade74de1e43a8156fa28cc469f07c
Taraszovics Péter <gtarapeti@gmail.com>
2019-06-07 10:04:56 +0200

    implemented service methods for daos

--- src/main/java/com/codecool/web/service/AttributesService.java
+++ src/main/java/com/codecool/web/service/AttributesService.java
@@ -1,4 +1,15 @@
 package com.codecool.web.service;
 
+import com.codecool.web.model.Attribure;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface AttributesService {
+    List<Attribure> getAll() throws SQLException, ServiceException;
+
+    Attribure getbyId(String attributeId) throws SQLException, ServiceException;
+
+    List<Attribure> getbyType(String type) throws SQLException, ServiceException;
 }
--- src/main/java/com/codecool/web/service/simple/SimpleAttributesService.java
+++ src/main/java/com/codecool/web/service/simple/SimpleAttributesService.java
@@ -1,6 +1,38 @@
 package com.codecool.web.service.simple;
 
+import com.codecool.web.dao.AttriburesDao;
+import com.codecool.web.model.Attribure;
 import com.codecool.web.service.AttributesService;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class SimpleAttributesService implements AttributesService {
+    private final AttriburesDao attriburesDao;
+
+    public SimpleAttributesService(AttriburesDao attriburesDao) {
+        this.attriburesDao = attriburesDao;
+    }
+
+    @Override
+    public List<Attribure> getAll() throws SQLException, ServiceException {
+        return attriburesDao.findAll();
+    }
+
+    @Override
+    public Attribure getbyId(String attributeId) throws SQLException, ServiceException {
+        try {
+            return attriburesDao.findbyId(Integer.parseInt(attributeId));
+        }  catch (NumberFormatException ex) {
+            throw new ServiceException("Day id must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public List<Attribure> getbyType(String type) throws SQLException, ServiceException {
+        return attriburesDao.findbyType(type);
+    }
 }
