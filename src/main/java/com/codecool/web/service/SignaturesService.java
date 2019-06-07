f8b6758c8b39607be15b16125471d1fc22ff13f4
Taraszovics Péter <gtarapeti@gmail.com>
2019-06-07 12:30:26 +0200

    implemented dao methods to service

--- src/main/java/com/codecool/web/service/SignaturesService.java
+++ src/main/java/com/codecool/web/service/SignaturesService.java
@@ -1,4 +1,30 @@
 package com.codecool.web.service;
 
+import com.codecool.web.model.Product;
+import com.codecool.web.model.Signature;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface SignaturesService {
+    List<Signature> getAll() throws SQLException;
+
+    Signature getbyProId(String proId) throws SQLException, ServiceException;
+
+    Product getProductbyDeckId(String deckId) throws SQLException, ServiceException;
+
+    Product getProductbyTruckId(String truckId) throws SQLException, ServiceException;
+
+    Product getProductbyGripId(String gripId) throws SQLException, ServiceException;
+
+    Product getProductbyWheelId(String wheelId) throws SQLException, ServiceException;
+
+    Signature getbyDeckId(String deckId) throws SQLException, ServiceException;
+
+    Signature getbyTruckId(String truckId) throws SQLException, ServiceException;
+
+    Signature getbyGripId(String gripId) throws SQLException, ServiceException;
+
+    Signature getbyWheelId(String wheelId) throws SQLException, ServiceException;
 }
--- src/main/java/com/codecool/web/service/simple/SimpleSignaturesService.java
+++ src/main/java/com/codecool/web/service/simple/SimpleSignaturesService.java
@@ -1,6 +1,122 @@
 package com.codecool.web.service.simple;
 
+import com.codecool.web.dao.SignaturesDao;
+import com.codecool.web.model.Product;
+import com.codecool.web.model.Signature;
 import com.codecool.web.service.SignaturesService;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class SimpleSignaturesService implements SignaturesService {
+    private final SignaturesDao signaturesDao;
+
+    public SimpleSignaturesService(SignaturesDao signaturesDao) {
+        this.signaturesDao = signaturesDao;
+    }
+
+    @Override
+    public List<Signature> getAll() throws SQLException {
+        return signaturesDao.findAll();
+    }
+
+    @Override
+    public Signature getbyProId(String proId) throws SQLException, ServiceException {
+        try {
+            return signaturesDao.findbyProId(Integer.parseInt(proId));
+        }  catch (NumberFormatException ex) {
+            throw new ServiceException("proId must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public Product getProductbyDeckId(String deckId) throws SQLException, ServiceException {
+        try {
+            return signaturesDao.findProductbyDeckId(Integer.parseInt(deckId));
+        }  catch (NumberFormatException ex) {
+            throw new ServiceException("deckId must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public Product getProductbyTruckId(String truckId) throws SQLException, ServiceException {
+        try {
+            return signaturesDao.findProductbyTruckId(Integer.parseInt(truckId));
+        }  catch (NumberFormatException ex) {
+            throw new ServiceException("truckId must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public Product getProductbyGripId(String gripId) throws SQLException, ServiceException {
+        try {
+            return signaturesDao.findProductbyGripId(Integer.parseInt(gripId));
+        }  catch (NumberFormatException ex) {
+            throw new ServiceException("gripId must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public Product getProductbyWheelId(String wheelId) throws SQLException, ServiceException {
+        try {
+            return signaturesDao.findProductbyWheelId(Integer.parseInt(wheelId));
+        }  catch (NumberFormatException ex) {
+            throw new ServiceException("wheelId must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public Signature getbyDeckId(String deckId) throws SQLException, ServiceException {
+        try {
+            return signaturesDao.findbyDeckId(Integer.parseInt(deckId));
+        }  catch (NumberFormatException ex) {
+            throw new ServiceException("deckId must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public Signature getbyTruckId(String truckId) throws SQLException, ServiceException {
+        try {
+            return signaturesDao.findbyTruckId(Integer.parseInt(truckId));
+        }  catch (NumberFormatException ex) {
+            throw new ServiceException("truckId must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public Signature getbyGripId(String gripId) throws SQLException, ServiceException {
+        try {
+            return signaturesDao.findbyGripId(Integer.parseInt(gripId));
+        }  catch (NumberFormatException ex) {
+            throw new ServiceException("gripId must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public Signature getbyWheelId(String wheelId) throws SQLException, ServiceException {
+        try {
+            return signaturesDao.findbyWheelId(Integer.parseInt(wheelId));
+        }  catch (NumberFormatException ex) {
+            throw new ServiceException("wheelId must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
 }
