06b04066ee935ce5f0cbe8fa2570043d08673f3e
Taraszovics Péter <gtarapeti@gmail.com>
2019-06-07 12:20:06 +0200

    implemented dao methods to service

--- src/main/java/com/codecool/web/service/ProductsService.java
+++ src/main/java/com/codecool/web/service/ProductsService.java
@@ -1,4 +1,24 @@
 package com.codecool.web.service;
 
+import com.codecool.web.model.Product;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ProductsService {
+
+    List<Product> getAll() throws SQLException, SQLException;
+
+    List<Product> getbyName(String name) throws SQLException, SQLException;
+
+    List<Product> getbyBrand(String brand) throws SQLException, SQLException;
+
+    Product getbyId(String prodcutId) throws SQLException, SQLException, ServiceException;
+
+    int countAllProducts() throws SQLException, SQLException;
+
+    Product[] getbyPriceASC() throws SQLException, SQLException;
+
+    Product[] getbyPriceDESC() throws SQLException, SQLException;
 }
--- src/main/java/com/codecool/web/service/simple/SimpleProductsService.java
+++ src/main/java/com/codecool/web/service/simple/SimpleProductsService.java
@@ -1,6 +1,59 @@
 package com.codecool.web.service.simple;
 
+import com.codecool.web.dao.ProductsDao;
+import com.codecool.web.model.Product;
 import com.codecool.web.service.ProductsService;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class SimpleProductsService implements ProductsService {
+    private final ProductsDao productsDao;
+
+
+    public SimpleProductsService(ProductsDao productsDao) {
+        this.productsDao = productsDao;
+    }
+
+    @Override
+    public List<Product> getAll() throws SQLException, SQLException {
+        return productsDao.findAll();
+    }
+
+    @Override
+    public List<Product> getbyName(String name) throws SQLException, SQLException {
+        return productsDao.findbyName(name);
+    }
+
+    @Override
+    public List<Product> getbyBrand(String brand) throws SQLException, SQLException {
+        return productsDao.findbyBrand(brand);
+    }
+
+    @Override
+    public Product getbyId(String prodcutId) throws SQLException, ServiceException {
+        try {
+            return productsDao.findbyId(Integer.parseInt(prodcutId));
+        }  catch (NumberFormatException ex) {
+            throw new ServiceException("prodcutId must be an integer");
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public int countAllProducts() throws SQLException, SQLException {
+        return productsDao.countAllProducts();
+    }
+
+    @Override
+    public Product[] getbyPriceASC() throws SQLException, SQLException {
+        return productsDao.findbyPriceASC();
+    }
+
+    @Override
+    public Product[] getbyPriceDESC() throws SQLException, SQLException {
+        return productsDao.findbyPriceDESC();
+    }
 }
