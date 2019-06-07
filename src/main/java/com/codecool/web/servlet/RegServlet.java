e11dd9d0dcf513875e51827a041b8693e0dfcf8e
Taraszovics Péter <gtarapeti@gmail.com>
2019-06-07 13:13:18 +0200

    created reg servlet, not tested

--- src/main/java/com/codecool/web/servlet/RegServlet.java
+++ src/main/java/com/codecool/web/servlet/RegServlet.java
@@ -1,4 +1,40 @@
 package com.codecool.web.servlet;
 
-public class RegServlet {
+import com.codecool.web.dao.UserDao;
+import com.codecool.web.dao.database.DatabaseUserDao;
+import com.codecool.web.model.User;
+import com.codecool.web.service.UserService;
+import com.codecool.web.service.exception.ServiceException;
+import com.codecool.web.service.simple.SimpleUsersService;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/register")
+public class RegServlet extends AbstractServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try (Connection connection = getConnection(req.getServletContext())) {
+            String name = req.getParameter("name");
+            String email = req.getParameter("email");
+            String password = req.getParameter("password");
+
+            UserDao userDao = new DatabaseUserDao(connection);
+            UserService userService = new SimpleUsersService(userDao);
+            userService.addNewUser(name, email, password);
+
+            sendMessage(resp, HttpServletResponse.SC_OK, null);
+
+        } catch (ServiceException ex) {
+            sendMessage(resp, HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
+
+        } catch (SQLException ex) {
+            handleSqlError(resp, ex);
+
+        }
+    }
 }
