461bb635706d85a4d46c621846a364756da73da1
Taraszovics Péter <gtarapeti@gmail.com>
2019-07-04 14:46:36 +0200

    data from servlet now arrives

--- src/main/webapp/products.js
+++ src/main/webapp/products.js
@@ -1,9 +1,39 @@
 function onAllProductsResponse() {
     if (this.status === OK) {
-        let json = JSON.parse(this.responseText);
-        console.log(json);
+        let productDto = JSON.parse(this.responseText);
+        let products = productDto.products;
+        showTable(products);
     } else {
         onOtherResponse(schedulesContentDivEl, this);
     }
+}
+
+function showTable(products) {
+    let table = document.getElementById("product-table");
+    if (table == null) {
+        document.getElementById('product-content').appendChild(generateTable(table, products ));
+    } else {
+        table.remove();
+        document.getElementById('product-content').appendChild(generateTable(table, products ));
+    }
+    showContents(['profile-content', 'topnav', 'product-content']);
+}
+
+
+function generateTable(table, products) {
+    if (!table) table = document.createElement('table');
+
+    table.setAttribute('id', "product-table");
+    table.setAttribute('border', "1px");
+
+    for (let i = 0; i < products.length; i++) {
+        let product = products[i];
+        let tableDEl = document.createElement('td');
+        tableDEl.innerHTML = product.id;
+        let tableREl = document.createElement('tr');
+        tableREl.appendChild(tableDEl);
+        table.appendChild(tableREl);
+    }
+    return table;
 
 }
