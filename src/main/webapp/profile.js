80b65c68db7d132f80442295b66572766e5cebd2
Taraszovics Péter <gtarapeti@gmail.com>
2019-07-05 13:12:22 +0200

    moved function to corresponsing .js

--- src/main/webapp/profile.js
+++ src/main/webapp/profile.js
@@ -1,16 +1,3 @@
-function onAllProductsClicked() {
-    const params = new URLSearchParams();
-    const typeId = "all";
-    params.append('typeId', typeId);
-
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', onAllProductsResponse);
-    xhr.addEventListener('error', onNetworkError);
-    xhr.open('GET', 'product?' + params.toString());
-    xhr.send();
-}
-
-
 function onProfileLoad(user) {
     clearMessages();
     showContents(['profile-content', 'topnav']);
