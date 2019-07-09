afa80d62d12073c79a4da2dc7685513bf1e61248
Taraszovics Péter <gtarapeti@gmail.com>
2019-07-09 15:01:02 +0200

    doPost method posts in db sends ok status back

--- src/main/java/com/codecool/web/servlet/CartServlet.java
+++ src/main/java/com/codecool/web/servlet/CartServlet.java
@@ -1,11 +1,16 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.dao.OrdersDao;
 import com.codecool.web.dao.ProductsDao;
+import com.codecool.web.dao.database.DatabaseOrdersDao;
 import com.codecool.web.dao.database.DatabaseProductsDao;
-import com.codecool.web.model.Product;
+import com.codecool.web.model.User;
+import com.codecool.web.service.OrdersService;
 import com.codecool.web.service.ProductsService;
 import com.codecool.web.service.exception.ServiceException;
+import com.codecool.web.service.simple.SimpleOrdersService;
 import com.codecool.web.service.simple.SimpleProductsService;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,19 +18,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
+
 import java.sql.SQLException;
+import java.util.Date;
 
 @WebServlet("/cart")
 public class CartServlet extends AbstractServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try (Connection connection = getConnection(req.getServletContext())) {
+            OrdersDao ordersDao = new DatabaseOrdersDao(connection);
+            OrdersService ordersService = new SimpleOrdersService(ordersDao);
+
+            ProductsDao productsDao = new DatabaseProductsDao(connection);
+            ProductsService productsService = new SimpleProductsService(productsDao);
 
             String productId = req.getParameter("productId");
+
+            User user = (User) req.getSession().getAttribute("user");
+            int userId = user.getId();
+
+            long currentTimeMillis = System.currentTimeMillis();
+
+            int price = productsService.getbyId(productId).getPrice();
+
+            ordersService.order(userId, productId, price, currentTimeMillis);
 
             sendMessage(resp, HttpServletResponse.SC_OK, null);
         } catch (SQLException e) {
             handleSqlError(resp, e);
+        } catch (ServiceException e) {
+            e.printStackTrace();
         }
     }
 }
