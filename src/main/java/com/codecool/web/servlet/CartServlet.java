35ad6649302edfa0967bfaa25560a5db0b071a70
Taraszovics Péter <gtarapeti@gmail.com>
2019-07-09 15:26:45 +0200

    doGet method to get orders for current user

--- src/main/java/com/codecool/web/servlet/CartServlet.java
+++ src/main/java/com/codecool/web/servlet/CartServlet.java
@@ -4,6 +4,7 @@ import com.codecool.web.dao.OrdersDao;
 import com.codecool.web.dao.ProductsDao;
 import com.codecool.web.dao.database.DatabaseOrdersDao;
 import com.codecool.web.dao.database.DatabaseProductsDao;
+import com.codecool.web.model.Order;
 import com.codecool.web.model.User;
 import com.codecool.web.service.OrdersService;
 import com.codecool.web.service.ProductsService;
@@ -20,7 +21,7 @@ import java.io.IOException;
 import java.sql.Connection;
 
 import java.sql.SQLException;
-import java.util.Date;
+import java.util.List;
 
 @WebServlet("/cart")
 public class CartServlet extends AbstractServlet {
@@ -47,6 +48,27 @@ public class CartServlet extends AbstractServlet {
             sendMessage(resp, HttpServletResponse.SC_OK, null);
         } catch (SQLException e) {
             handleSqlError(resp, e);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (Connection connection = getConnection(req.getServletContext())) {
+            OrdersDao ordersDao = new DatabaseOrdersDao(connection);
+            OrdersService ordersService = new SimpleOrdersService(ordersDao);
+
+            User user = (User) req.getSession().getAttribute("user");
+            int userId = user.getId();
+
+            List<Order> orders = ordersService.findbyCustomerId(userId);
+
+            sendMessage(resp, HttpServletResponse.SC_OK, orders);
+        } catch (SQLException e) {
+            handleSqlError(resp, e);
+
+
         } catch (ServiceException e) {
             e.printStackTrace();
         }
