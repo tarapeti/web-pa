761e645759475209799312ca8686ef5a44cadaa1
Taraszovics Péter <gtarapeti@gmail.com>
2019-07-10 15:45:48 +0200

    displaying cart itmes

--- src/main/webapp/cart.js
+++ src/main/webapp/cart.js
@@ -1,44 +1,78 @@
 function putinCart(productId) {
-    const params = new URLSearchParams();
+    sessionStorage.setItem(productId, productId);
+    putinCartResponse();
+
+
+/*    const params = new URLSearchParams();
     params.append('productId', productId);
 
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('load', putinCartResponse);
     xhr.addEventListener('error', onNetworkError);
     xhr.open('POST', 'cart?' + params.toString());
-    xhr.send();
+    xhr.send();*/
 
 }
 
 function putinCartResponse() {
-    if (this.status === OK) {
-        alert("Item put into cart");
-        showContents(['profile-content', 'topnav', 'product-display']);
-    } else {
-        onOtherResponse(schedulesContentDivEl, this);
-    }
+    alert("Item put into cart");
+    showContents(['profile-content', 'topnav', 'product-display']);
 }
 
 function onCartClikced(){
+    const params = new URLSearchParams();
+    const productIds = Object.keys(sessionStorage);
+    params.append('productIds', productIds);
+
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('load', onCartClikcedResponse);
     xhr.addEventListener('error', onNetworkError);
-    xhr.open('GET', 'cart?');
+    xhr.open('GET', 'cart?' + params.toString());
     xhr.send();
 
 }
 function onCartClikcedResponse() {
     if (this.status === OK) {
-        let orders = JSON.parse(this.responseText);
-        console.log(orders);
-        displayOrdersForUser(orders);
-        showContents(['profile-content', 'topnav', 'product-display']);
+        let cartItems = JSON.parse(this.responseText);
+        console.log(cartItems);
+        displayCart(cartItems.products);
+        showContents(['profile-content', 'topnav', 'cart-display']);
     } else {
         onOtherResponse(schedulesContentDivEl, this);
     }
 }
 
-function displayOrdersForUser(orders) {
+function displayCart(cartItems)  {
+    let table = document.getElementById("cart-table");
+    if (table == null) {
+        document.getElementById('cart-display').appendChild(generateSimpleTable(null, cartItems));
+    } else {
+        table.remove();
+        document.getElementById('cart-display').appendChild(generateSimpleTable(null, cartItems));
+    }
+}
+function generateSimpleTable(table, cartItems) {
+    if (!table) table = document.createElement('table');
 
+    table.setAttribute('id', "cart-table");
+    table.setAttribute('border', "1px");
+
+    for (let i = 0; i < cartItems.length; i++) {
+        let product = cartItems[i];
+        let name = document.createElement('p');
+        name.textContent = product.name;
+        let tableREl = document.createElement('tr');
+
+        let price = document.createElement('p');
+        price.textContent = product.price;
+        let priceTrEl = document.createElement('tr');
+
+        priceTrEl.appendChild(price);
+        tableREl.appendChild(name);
+        table.appendChild(tableREl);
+        table.appendChild(priceTrEl);
+
+    }
+    return table;
 
 }
