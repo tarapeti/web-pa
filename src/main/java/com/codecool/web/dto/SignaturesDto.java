9487e6c2ed553cb27f0c1f2a257849a26d6ab79b
Taraszovics Péter <gtarapeti@gmail.com>
2019-07-14 14:18:10 +0200

    transfering

--- src/main/java/com/codecool/web/dto/SignaturesDto.java
+++ src/main/java/com/codecool/web/dto/SignaturesDto.java
@@ -1,4 +1,17 @@
 package com.codecool.web.dto;
 
+import com.codecool.web.model.Signature;
+
+import java.util.List;
+
 public class SignaturesDto {
+    private final List<Signature> signatures;
+
+    public SignaturesDto(List<Signature> signatures) {
+        this.signatures = signatures;
+    }
+
+    public List<Signature> getSignatures() {
+        return signatures;
+    }
 }
