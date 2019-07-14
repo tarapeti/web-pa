d10891b761a83f14e9c2bcda44b2c55c5befcde0
Taraszovics Péter <gtarapeti@gmail.com>
2019-07-14 14:18:29 +0200

    doGet implemented

--- src/main/java/com/codecool/web/servlet/SignaturesServlet.java
+++ src/main/java/com/codecool/web/servlet/SignaturesServlet.java
@@ -1,5 +1,11 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.dao.SignaturesDao;
+import com.codecool.web.dao.database.DatabaseSignaturesDao;
+import com.codecool.web.model.Signature;
+import com.codecool.web.service.SignaturesService;
+import com.codecool.web.service.simple.SimpleSignaturesService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,14 +13,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/signature")
 public class SignaturesServlet extends AbstractServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try (Connection connection = getConnection(req.getServletContext())){
+            SignaturesDao signaturesDao = new DatabaseSignaturesDao(connection);
+            SignaturesService signaturesService = new SimpleSignaturesService(signaturesDao);
 
-            sendMessage(resp, HttpServletResponse.SC_OK, null);
+            List<Signature> signatures = signaturesService.getAll();
+
+            sendMessage(resp, HttpServletResponse.SC_OK, signatures);
         } catch (SQLException e) {
             handleSqlError(resp, e);
         }
